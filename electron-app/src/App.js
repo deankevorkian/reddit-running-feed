9392ae34bfc542f7b5184838bf5fdbe424e84a19
deankevorkian <deankevor@gmail.com>
2017-05-24 00:48:27 +0300

    Added icon for reddit.

--- electron-app/src/App.js
+++ electron-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import reddit from './reddit.png';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +8,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={reddit} className="App-logo" alt="logo" />
+          <h2>Reddit Running Feed</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {/* To get started, edit <code>src/App.js</code> and save to reload. */}
         </p>
       </div>
     );
