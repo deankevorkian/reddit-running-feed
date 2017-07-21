edfc7a1bf79810f3e76518c9575a51cbc0e321ad
deankevorkian <deankevor@gmail.com>
2017-07-21 16:31:56 +0300

    Major changes. Finally added support for localStorage of the browser, to store the tokens received from Reddit's API. From now on, everytime a user logs in the app, we check if his localStorage contains a refreshToken. If he does - we use it! If not, we generate a new one and store it there.

--- electron-app/src/core/reddit/SubredditService.js
+++ electron-app/src/core/reddit/SubredditService.js
@@ -2,45 +2,87 @@
 
 import snoowrap from 'snoowrap';
 
+const appUserAgent: string = "Reddit Running Feed";
+const appClientId: string = "B9MiM6YU7XRzmA";
+const appClientSecret: string = "";
+
 export default class SubredditService {
   authCode: string;
+  agent: snoowrap;
+  userRefreshToken: ?string;
 
   constructor() {
-    let code = new URL(window.location.href).searchParams.get('code');
-
-    if (code === null || code === undefined) {
-      let authenticationUrl = snoowrap.getAuthUrl({
-        clientId: 'B9MiM6YU7XRzmA',
-        scope: ['identity', 'wikiread', 'wikiedit', 'read'],
-        redirectUri: 'http://localhost:3000/',
-        permanent: true,
-        state: 'fe211bebc52eb3da9bef8db6e63104d3' // a random string, this could be validated when the user is redirected back
-      });
-      // --> 'https://www.reddit.com/api/v1/authorize?client_id=foobarbaz&response_type=code&state= ...'
-
-      window.location = authenticationUrl; // send the user to the authentication url
+    let userRefreshToken: ?string = localStorage.getItem("user_refresh_token");
+    if (userRefreshToken) {
+      this.userRefreshToken = userRefreshToken;
     }
     else {
-      this.authCode = code;
+      let code = new URL(window.location.href).searchParams.get('code');
+
+      if (code) {
+        this.authCode = code;
+      }
+      else {
+        let authenticationUrl = snoowrap.getAuthUrl({
+          clientId: 'B9MiM6YU7XRzmA',
+          scope: ['identity', 'wikiread', 'wikiedit', 'read'],
+          redirectUri: 'http://localhost:3000/',
+          permanent: true,
+          state: 'fe211bebc52eb3da9bef8db6e63104d3' // a random string, this could be validated when the user is redirected back
+        });
+        // --> 'https://www.reddit.com/api/v1/authorize?client_id=foobarbaz&response_type=code&state= ...'
+
+        window.location = authenticationUrl; // send the user to the authentication url
+      }
+    }
+
+
+  }
+
+  getAgent() : Promise<snoowrap> {
+    if (this.agent) {
+      return Promise.resolve(this.agent);
+    }
+    else {
+      if (this.userRefreshToken) {
+        return this.createPromiseFromRefreshToken();
+      }
+      else if (this.authCode) {
+        return snoowrap.fromAuthCode({
+          code: this.authCode,
+          userAgent: 'My app',
+          clientId: 'B9MiM6YU7XRzmA',
+          redirectUri: 'http://localhost:3000/'
+        }).then(snooAgent => {
+          this.userRefreshToken = snooAgent.refreshToken;
+          localStorage.setItem("user_refresh_token", snooAgent.refreshToken);
+          console.log("VISITORS!");
+        }).then(() => {
+          return this.createPromiseFromRefreshToken();
+        });
+      }
+      else {
+        return Promise.reject("No auth code received. Authenticate via reddit first.");
+      }
     }
   }
 
-  getAgent() : Promise<any> {
-    if (this.authCode) {
-      return snoowrap.fromAuthCode({
-        code: this.authCode,
-        userAgent: 'My app',
-        clientId: 'B9MiM6YU7XRzmA',
-        redirectUri: 'http://localhost:3000/'
+  createPromiseFromRefreshToken() : Promise<snoowrap> {
+    return new Promise((resolve, reject) => {
+      let agent = new snoowrap({
+        userAgent: appUserAgent,
+        clientId: appClientId,
+        clientSecret: appClientSecret,
+        refreshToken: this.userRefreshToken
       });
-      /*.then(r => {
-        // Now we have a requester that can access reddit through the user's account
-        this.redditAgent = r;
-        this.isInitialized = true;
-      });*/
-    }
-    else {
-      return Promise.reject("No auth code received. Authenticate via reddit first.");
-    }
+
+      if (agent) {
+        this.agent = agent;
+        resolve(this.agent);
+      }
+      else {
+        reject("Failed to create agent.");
+      }
+    });
   }
 }
